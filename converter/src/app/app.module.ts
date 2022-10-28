48651fe7a098eda0bcfc353d365d74d9cb0c8fa0
Ivan Draganov <draganov.ivan.an@gmail.com>
2022-10-28 12:07:53 +0300

    started select

--- converter/src/app/app.module.ts
+++ converter/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SelectComponent } from './select/select.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SelectComponent
   ],
   imports: [
     BrowserModule,

e67cb74afeea145a1f93aab91691ceb690dc64e5
Ivan Draganov <draganov.ivan.an@gmail.com>
2022-10-28 15:58:05 +0300

    select toggle working

--- converter/src/app/select/select.component.ts
+++ converter/src/app/select/select.component.ts
@@ -6,46 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select.component.css']
 })
 export class SelectComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
 
-
-    this.renderSelect();
-    // this.initEventListeners();
   }
 
-  selectDiv: any
+  selectDiv: any;
 
-  renderSelect() {
-    this.selectDiv = this.getSelectTemplate();
+  toggleF: boolean = false;
+  toggleS: boolean = false;
+
+  clickHandlerFirst() {
+    this.toggleF = !this.toggleF
+  }
+  clickHandlerSecond() {
+    this.toggleS = !this.toggleS
   }
 
   getSelectTemplate() {
 
   }
 
-  // initEventListeners() {
-  //   document.querySelector('.select__list').addEventListener('click', (event) => {
-  //     this.selectChoose(event.target.innerText, event.target.value);
-  //   })
-
-  //   document.querySelector(".select").addEventListener('click', (event) => {
-  //     this.classListInput = ["input__value", "input__wrap", "select__icon-use", "select__icon"];
-
-  //     if (this.classListInput.includes(event.target.className)
-  //       || this.classListInput.includes(event.target.classList.value)) {
-  //       this.selectToggler();
-  //     }
-  //   })
-  // }
-
-  // selectToggler() {
-  //   this.selectDiv.classList.toggle('is-active');
-
-  //   this.iconToggler();
-  // }
 
   // selectChoose(text, value) {
   //   document.querySelector('.input__value').value = text;

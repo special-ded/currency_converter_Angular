fb1252e2f3c4d5592d5f077c32fb14ed9c973fa2
Ivan Draganov <draganov.ivan.an@gmail.com>
2022-10-28 10:50:46 +0300

    fetching data

--- converter/src/app/header/header.component.ts
+++ converter/src/app/header/header.component.ts
@@ -3,13 +3,41 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.getExchangeRate();
   }
 
+  usd: number = 0;
+  json = [];
+
+  async aaa() {
+    this.json = await this.getExchangeRate();
+    this.usd = await this.getExchangeRate();
+    console.log();
+  }
+
+  //  eur = json[25].rate
+
+  async getExchangeRate() {
+    try {
+      const USDurl =
+        'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json?valcode=USD&date=20201028';
+      const UERurl =
+        'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json?valcode=EUR&date=20201028';
+      const url =
+        'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json';
+      const response = await fetch(url);
+      const json = await response.json();
+      console.log(json);
+
+      return json;
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
 }

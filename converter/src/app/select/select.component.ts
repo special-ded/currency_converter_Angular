457e5e9f95c0b07118b6ab1ec295534b23de1555
Ivan Draganov <draganov.ivan.an@gmail.com>
2022-10-28 16:34:17 +0300

    some changes

--- converter/src/app/select/select.component.ts
+++ converter/src/app/select/select.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
@@ -16,42 +18,14 @@ export class SelectComponent implements OnInit {
 
   toggleF: boolean = false;
   toggleS: boolean = false;
+  // href: string = logo
 
   clickHandlerFirst() {
     this.toggleF = !this.toggleF
+
   }
   clickHandlerSecond() {
     this.toggleS = !this.toggleS
   }
-
-  getSelectTemplate() {
-
-  }
-
-
-  // selectChoose(text, value) {
-  //   document.querySelector('.input__value').value = text;
-
-  //   this.selectDiv.classList.remove('is-active');
-  //   this.iconToggler();
-  //   this.onSelectChange(value);
-  // }
-
-  // iconToggler() {
-  //   this.paginatorIconLink = document.querySelector(".select__icon-use");
-
-  //   this.paginatorIconLink.href.baseVal = "./assets/images/sprite.svg#icon-arrow-default";
-
-  //   if (this.selectDiv.className === "select is-active") {
-  //     this.paginatorIconLink.href.baseVal = "./assets/images/sprite.svg#icon-arrow-active";
-  //   }
-  // }
-
-  // async onSelectChange(value) {
-  //   const app = new App()
-  //   const data = await app.setSliderData(value);
-
-  //   app.initSlider(data)
-  // }
 
 }

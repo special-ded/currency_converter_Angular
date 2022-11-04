1dd35936a7cae6a42ab604cda7153d38c6673bd0
Ivan Draganov <draganov.ivan.an@gmail.com>
2022-11-04 10:18:12 +0200

    types fixed

--- converter/src/app/select/select.component.ts
+++ converter/src/app/select/select.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ExchRateObj } from '../exchrateobj';
 import { RatesService } from '../rates.service';
 
+
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
@@ -20,66 +21,65 @@ export class SelectComponent implements OnInit {
   uah: string = "UAH";
   toggleF: boolean = false;
   toggleS: boolean = false;
-  firstCurencyName: string = "UAH";
-  secondCurencyName: string = "USD";
+  firstCurrencyName: string = "UAH";
+  secondCurrencyName: string = "USD";
   firstValue: number = 1;
   secondValue: number;
+  secondCurrencyRate: number | undefined;
+  firstCurrencyRate: number | undefined = 0;
 
 
-  secondCurencyRate: number | undefined;
-  firstCurencyRate: number | undefined = 0;
-
-  getRates(event: any) {
+  getRates(event: Event) {
     this.exchRates.reqRates()
       .subscribe((val: ExchRateObj[]) => {
 
         this.rates = val;
-        this.secondCurencyRate = this.rates.find(el => el.cc === this.secondCurencyName)?.rate;
-        this.firstCurencyRate = this.rates.find(el => el.cc === this.firstCurencyName)?.rate;
+        this.secondCurrencyRate = this.rates.find(el => el.cc === this.secondCurrencyName)?.rate;
+        this.firstCurrencyRate = this.rates.find(el => el.cc === this.firstCurrencyName)?.rate;
 
-        if (event.target.name === this.secondInputValue) {
-          this.secondInputHandler(this.firstCurencyRate, this.secondCurencyRate, this.uah)
+        if ((<HTMLTextAreaElement>event?.target).name === this.secondInputValue) {
+          this.secondInputHandler(this.firstCurrencyRate, this.secondCurrencyRate, this.uah)
           return
         }
-        this.firstInputHandler(this.firstCurencyRate, this.secondCurencyRate, this.uah)
+        this.firstInputHandler(this.firstCurrencyRate, this.secondCurrencyRate, this.uah)
       })
   }
 
-  clickHandlerFirst(event?: any) {
-    this.toggleF = !this.toggleF
+  clickHandlerFirst(event?: MouseEvent) {
+    this.toggleF = !this.toggleF;
 
-    if (event?.target.innerText) {
-      this.firstCurencyName = event?.target.innerText
+    if ((<HTMLTextAreaElement>event?.target).innerText) {
+      this.firstCurrencyName = (<HTMLTextAreaElement>event?.target).innerText
     }
     if (event) this.inputHandler(event)
   }
 
-  clickHandlerSecond(event?: any) {
-    this.toggleS = !this.toggleS
+  clickHandlerSecond(event?: MouseEvent) {
+    this.toggleS = !this.toggleS;
 
-    if (event?.target.innerText) {
-      this.secondCurencyName = event?.target.innerText
+    if ((<HTMLTextAreaElement>event?.target).innerText) {
+      this.secondCurrencyName = (<HTMLTextAreaElement>event?.target).innerText
     }
     if (event) this.inputHandler(event)
   }
 
-  inputHandler(event: any) {
+  inputHandler(event: Event) {
     this.getRates(event)
   }
 
   firstInputHandler(firstCurencyRate: number | undefined, secondCurencyRate: number | undefined, uah: string) {
 
-    if (this.firstCurencyName === this.secondCurencyName) {
+    if (this.firstCurrencyName === this.secondCurrencyName) {
       this.secondValue = this.firstValue
       return
     }
 
-    if (this.firstCurencyName === uah) {
+    if (this.firstCurrencyName === uah) {
       if (secondCurencyRate) this.secondValue = this.firstValue / secondCurencyRate
       return
     }
 
-    if (this.secondCurencyName === uah) {
+    if (this.secondCurrencyName === uah) {
       if (firstCurencyRate) this.secondValue = this.firstValue * firstCurencyRate
       return
     }
@@ -89,20 +89,19 @@ export class SelectComponent implements OnInit {
 
   secondInputHandler(firstCurencyRate: number | undefined, secondCurencyRate: number | undefined, uah: string) {
 
-    if (this.secondCurencyName === this.firstCurencyName) {
+    if (this.secondCurrencyName === this.firstCurrencyName) {
       this.firstValue = this.secondValue
       return
     }
 
-    if (this.secondCurencyName === uah) {
+    if (this.secondCurrencyName === uah) {
       if (firstCurencyRate) this.firstValue = this.secondValue / firstCurencyRate
     }
 
-    if (this.firstCurencyName === uah) {
+    if (this.firstCurrencyName === uah) {
       if (secondCurencyRate) this.firstValue = this.secondValue * secondCurencyRate
     }
 
     if (secondCurencyRate && firstCurencyRate) this.firstValue = this.secondValue * secondCurencyRate / firstCurencyRate
   }
-
 }
